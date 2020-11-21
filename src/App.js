69d30d1a6afa3fcfd695a695111593c2a3aa4f40
Gabrielle <g.devanz@gmail.com>
2020-11-21 18:33:39 +0100

    Import shuffleArray from module

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
 import GAME_DATA from "./data/gamedata";
 //import GAME_DATA_TEST from "../data/gamedata-test";
-import utils from "./utils";
+import "./App.css";
+import { shuffleArray } from "./utils";
 
 // Replace GAME_DATA_TEST with GAME_DATA for actual questions
-const GAME_QUESTIONS = utils.shuffleArray(GAME_DATA);
+const GAME_QUESTIONS = shuffleArray(GAME_DATA);
 const MAX_ID = GAME_QUESTIONS.length - 1;
+
+const Titre = () => <div>Pathologie ou figure de style ?</div>;
 
 const Question = (props) => <div>{props.question}</div>;
 
@@ -80,26 +83,34 @@ const Figures = () => {
 
   return (
     <div className="body">
-      <div className="score">Score : {score}</div>
-      <div>
-        <Question
-          question={
-            gameStatus === "actif" ? question.value : "Merci d'avoir joué !"
-          }
-        />
+      <div className="title">
+        <Titre />
       </div>
-      <div className="answerzone">
-        <FigureButton onClick={onButtonClick} />
-        <PathologieButton onClick={onButtonClick} />
-        {gameStatus === "actif" && <YourAnswer correctAnswer={isCorrect} />}
-      </div>
-      <div>
-        {questionId > 0 && (
-          <Gotowikipage
-            onClick={goToPage}
-            keyword={GAME_QUESTIONS[questionId - 1].value}
+      <div className="middle">
+        <div className="secretzone">blabla</div>
+        <div className="question">
+          <Question
+            question={
+              gameStatus === "actif" ? question.value : "Merci d'avoir joué !"
+            }
           />
-        )}
+        </div>
+        <div className="score">Score : {score}</div>
+      </div>
+      <div className="lower">
+        <div className="answerzone">
+          <FigureButton onClick={onButtonClick} />
+          <PathologieButton onClick={onButtonClick} />
+          {gameStatus === "actif" && <YourAnswer correctAnswer={isCorrect} />}
+        </div>
+        <div>
+          {questionId > 0 && (
+            <Gotowikipage
+              onClick={goToPage}
+              keyword={GAME_QUESTIONS[questionId - 1].value}
+            />
+          )}
+        </div>
       </div>
     </div>
   );
--- src/utils.js
+++ src/utils.js
@@ -1,15 +1,10 @@
-const utils = {
-  // À améliorer : ne pas modifier le tableau ?
-  shuffleArray: (array) => {
-    var shuffledArray = [...array];
-    for (var i = shuffledArray.length - 1; i > 0; i--) {
-      var j = Math.floor(Math.random() * (i + 1));
-      var temp = shuffledArray[i];
-      shuffledArray[i] = shuffledArray[j];
-      shuffledArray[j] = temp;
-    }
-    return shuffledArray;
-  },
+export const shuffleArray = (array) => {
+  var shuffledArray = [...array];
+  for (var i = shuffledArray.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = shuffledArray[i];
+    shuffledArray[i] = shuffledArray[j];
+    shuffledArray[j] = temp;
+  }
+  return shuffledArray;
 };
-
-export default utils;
