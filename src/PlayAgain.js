1a7d0944c3cee843f20e420c94d1dc36df61447e
Gabrielle <g.devanz@gmail.com>
2020-11-25 10:38:54 +0100

    Afficher le score final en fin de partie

--- src/PlayAgain.js
+++ src/PlayAgain.js
@@ -1,10 +1,15 @@
 import React from "react";
 
+// Enlever le nombre de questions en dur !
+
 const PlayAgain = (props) => (
   <div className="game-done">
     <div className="game-done-panel">
       <div className="message">Merci d'avoir joué !</div>
-      <div className="final-score"> {props.score} </div>
+      <div className="final-score">
+        <p>Votre score</p>
+        <div className="final-score-number"> {props.score} / X</div>
+      </div>
       <button onClick={props.startNewGame}>Recommencer</button>
     </div>
   </div>
