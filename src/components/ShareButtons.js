7dccda25aa097b89e3da7951f9b0e1c5743e4479
Gabrielle <g.devanz@gmail.com>
2020-11-26 23:25:37 +0100

    Ajouter les logos en svg

--- src/components/ShareButtons.js
+++ src/components/ShareButtons.js
@@ -1,6 +1,6 @@
 import { FacebookShareButton, TwitterShareButton } from "react-share";
-import facebookIcon from "../assets/facebook-icon.png";
-import twitterIcon from "../assets/twitter-icon.png";
+import facebookIcon from "../assets/facebook.svg";
+import twitterIcon from "../assets/twitter.svg";
 const ShareButtons = ({ title, url, twitterHandle, tags }) => {
   return (
     <div className="share-buttons">
@@ -9,14 +9,14 @@ const ShareButtons = ({ title, url, twitterHandle, tags }) => {
         quote="Show me the Google!"
         url={"https://www.google.com"}
       >
-        <img src={facebookIcon} alt="facebook-icon" />
+        <img src={facebookIcon} width="40px" alt="facebook-icon" />
       </FacebookShareButton>
       <TwitterShareButton
         className="twitter-button"
         quote="Show me the Google!"
         url={"https://www.google.com"}
       >
-        <img src={twitterIcon} alt="twitter-icon" />
+        <img src={twitterIcon} width="40px" alt="twitter-icon" />
       </TwitterShareButton>
     </div>
   );
