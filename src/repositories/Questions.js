3cb3cc5ff59bf3d50549dcf8cfe8e353d2983a72
Gabrielle <g.devanz@gmail.com>
2020-11-26 23:07:01 +0100

    Tester avec les données

--- src/repositories/Questions.js
+++ src/repositories/Questions.js
@@ -2,5 +2,5 @@ import GAME_DATA from "./gamedata";
 import GAME_DATA_TEST from "./gamedata-test";
 import { shuffleArray } from "../utils/shuffleArray";
 
-export const GAME_QUESTIONS = shuffleArray(GAME_DATA_TEST);
+export const GAME_QUESTIONS = shuffleArray(GAME_DATA);
 export const NB_QUESTIONS = GAME_QUESTIONS.length;
