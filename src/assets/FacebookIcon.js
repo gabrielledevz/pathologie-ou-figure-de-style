065b32ba2676d21fbdfe03dce8db6e53c7a35a7a
Gabrielle <g.devanz@gmail.com>
2020-11-27 13:22:24 +0100

    Alléger la définition des composants

--- src/assets/FacebookIcon.js
+++ src/assets/FacebookIcon.js
@@ -1,21 +1,19 @@
 import * as React from "react";
 
-function FacebookIcon(props) {
-  return (
-    <svg
-      data-name="Calque 1"
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 512 512"
-      {...props}
-    >
-      <title>{"iconfinder_Facebook_38138prime9"}</title>
-      <circle cx={256} cy={256} r={256} className="social-media-background" />
-      <path
-        className="social-media-main"
-        d="M272 184a7.56 7.56 0 018-8h32v-48h-56a39.61 39.61 0 00-40 40v48h-24v48h24v120h56V264h32l8-48h-40z"
-      />
-    </svg>
-  );
-}
+const FacebookIcon = (props) => (
+  <svg
+    data-name="Calque 1"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 512 512"
+    {...props}
+  >
+    <title>{"iconfinder_Facebook_38138prime9"}</title>
+    <circle cx={256} cy={256} r={256} className="social-media-background" />
+    <path
+      className="social-media-main"
+      d="M272 184a7.56 7.56 0 018-8h32v-48h-56a39.61 39.61 0 00-40 40v48h-24v48h24v120h56V264h32l8-48h-40z"
+    />
+  </svg>
+);
 
 export default FacebookIcon;
