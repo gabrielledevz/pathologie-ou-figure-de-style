86172b69e63f3ce748f7147dd27588b849ad12cb
Gabrielle <g.devanz@gmail.com>
2020-12-03 08:48:52 +0100

    Renommer les fichiers de données

--- src/repositories/questions.js
+++ src/repositories/questions.js
@@ -1,8 +1,8 @@
-import GAME_DATA from "./gamedata";
-import GAME_DATA_TEST from "./gamedata-test";
+import QUIZZ_DATA from "./quizz-data";
+import QUIZZ_DATA_TEST from "./quizz-data-test";
 import { shuffleArray } from "../utils/shuffleArray";
 
-export const QUESTION_DATABASE = shuffleArray(GAME_DATA_TEST);
+export const QUESTION_DATABASE = shuffleArray(QUIZZ_DATA);
 export const NB_QUESTIONS_PER_GAME = 15;
 export const MAX_GAME_NUMBER = Math.trunc(
   QUESTION_DATABASE.length / NB_QUESTIONS_PER_GAME
