ce077377917f5f584e78a64f1241de5c57db0523
Gabrielle <g.devanz@gmail.com>
2020-12-17 00:38:24 +0100

    Retirer les console.log avec la liste des questions

--- src/repositories/questions.js
+++ src/repositories/questions.js
@@ -14,6 +14,3 @@ export const NB_QUESTIONS_PER_GAME = testMode ? 5 : 15;
 export const MAX_GAME_NUMBER = Math.trunc(
   QUESTION_DATABASE.length / NB_QUESTIONS_PER_GAME
 );
-
-console.log("GAME_QUESTIONS");
-console.log(QUESTION_DATABASE);
